c3ba353a86d7a32b2019d7bc3d452a3cba376e9a
Luke Worley <luke.worley@colorado.edu>
2018-05-11 02:04:32 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -10,6 +10,9 @@ function destructivelyAddElementToBeginningOfArray(array, element){
 }
 
 function addElementToEndOfArray(array, element){
-  
+  return [...array, element]
 }
 
+function destructivelyAddElementToEndOfArray(array, element){
+  
+}
